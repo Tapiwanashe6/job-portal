286cd61ed096d8aae416771a5fec4ffbdf45ce87
Tapiwanashe Marufu <t.marufu@alustudent.com>
2025-11-25 15:48:37 +0200

    upate

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import 'dotenv/config'
 import connectDB from './config/db.js'
 import * as Sentry from "@sentry/node"
 import clerkWebhooks from "./controllers/webhooks.js"
+import serverless from 'serverless-http'
 
 // Initialize Express
 const app = express()
@@ -18,17 +19,22 @@ app.use(express.json())
 
 // Routes
 app.get('/', (req, res) => res.send("API Working"))
-app.get("/debug=sentry", (req, res) => {
+app.get("/debug-sentry", (req, res) => {
     throw new Error("My first Sentry error!")
 })
 app.post('/webhooks', clerkWebhooks)
 
+Sentry.setupExpressErrorHandler(app)
+
 // Port
 const PORT = process.env.PORT || 5000
 
-Sentry.setupExpressErrorHandler(app)
+// For local development
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
+}
 
-// Start server
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+// Export for Vercel serverless
+export default serverless(app)
